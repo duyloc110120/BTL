13f444cac8530989dd7eeb3b57f798f81a5fd552
tuananhhoang1805 <81757605+tuananhhoang1805@users.noreply.github.com>
2021-12-28 13:31:27 +0700

    update giao dien dang nhap dang xuat

--- Bookstore/index.py
+++ Bookstore/index.py
@@ -15,79 +15,82 @@ def home():
 def details():
     return render_template('product_details.html')
 
+
 #
-# @app.route('/login', methods=['get', 'post'])
-# def login():
-#     error_msg = ""
-#     if request.method.__eq__('POST'):
-#         try:
-#             username = request.form['username']
-#             password = request.form['password']
-#
-#             user = utils.check_user(username=username, password=password)
-#             if user:
-#                 login_user(user=user)
-#
-#                 next = request.args.get('next', 'home')
-#                 return redirect(url_for(next))
-#             else:
-#                 error_msg = "Chuong trinh dang co loi! Vui long quay lai sau!"
-#
-#         except Exception as ex:
-#             error_msg = str(ex)
-#
-#     return render_template('login.html', error_msg=error_msg)
-#
-#
-# @app.route('/register', methods=['get', 'post'])
-# def register():
-#     err_msg = ''
-#     if request.method.__eq__('POST'):
-#         name = request.form.get('name')
-#         username = request.form.get('username')
-#         password = request.form.get('password')
-#         confirm = request.form.get('confirm')
-#         email = request.form.get('email')
-#
-#         if password.strip().__eq__(confirm.strip()):
-#             file = request.files.get('avatar')
-#             avatar = None
-#             if file:
-#                 res = cloudinary.uploader.upload(file)
-#                 avatar = res['secure_url']
-#
-#             try:
-#                 utils.create_user(name=name, password=password,
-#                                   username=username, email=email,
-#                                   avatar=avatar)
-#
-#                 return redirect(url_for('login'))
-#             except Exception as ex:
-#                 err_msg = 'Da co loi xay ra: ' + str(ex)
-#         else:
-#             err_msg = 'Mat khau KHONG khop!'
-#
-#     return render_template('register.html', err_msg=err_msg)
+@app.route('/login', methods=['get', 'post'])
+def login():
+    #     error_msg = ""
+    #     if request.method.__eq__('POST'):
+    #         try:
+    #             username = request.form['username']
+    #             password = request.form['password']
+    #
+    #             user = utils.check_user(username=username, password=password)
+    #             if user:
+    #                 login_user(user=user)
+    #
+    #                 next = request.args.get('next', 'home')
+    #                 return redirect(url_for(next))
+    #             else:
+    #                 error_msg = "Chuong trinh dang co loi! Vui long quay lai sau!"
+    #
+    #         except Exception as ex:
+    #             error_msg = str(ex)
+    #
+    return render_template('login.html')
+
+
 #
 #
-# @app.route('/user-logout')
-# def signout():
-#     logout_user()
-#     return redirect(url_for('login'))
-#
-#
-# @app.route("/admin-login", methods=['post'])
-# def admin_login():
-#     username = request.form.get('username')
-#     password = request.form.get('password')
-#
-#     user = utils.check_user(username=username, password=password, role=UserRole.ADMIN)
-#     if user:
-#         login_user(user=user)
-#
-#     return redirect('/admin')
-#
-#
+@app.route('/register', methods=['get', 'post'])
+def register():
+    #     err_msg = ''
+    #     if request.method.__eq__('POST'):
+    #         name = request.form.get('name')
+    #         username = request.form.get('username')
+    #         password = request.form.get('password')
+    #         confirm = request.form.get('confirm')
+    #         email = request.form.get('email')
+    #
+    #         if password.strip().__eq__(confirm.strip()):
+    #             file = request.files.get('avatar')
+    #             avatar = None
+    #             if file:
+    #                 res = cloudinary.uploader.upload(file)
+    #                 avatar = res['secure_url']
+    #
+    #             try:
+    #                 utils.create_user(name=name, password=password,
+    #                                   username=username, email=email,
+    #                                   avatar=avatar)
+    #
+    #                 return redirect(url_for('login'))
+    #             except Exception as ex:
+    #                 err_msg = 'Da co loi xay ra: ' + str(ex)
+    #         else:
+    #             err_msg = 'Mat khau KHONG khop!'
+    #
+    return render_template('register.html')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route("/admin-login", methods=['post'])
+def admin_login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    user = utils.check_user(username=username, password=password, role=UserRole.ADMIN)
+    if user:
+        login_user(user=user)
+
+    return redirect('/admin')
+
+
 @loginn.user_loader
 def load_user(user_id):
     return utils.get_user_by_id(user_id=user_id)
